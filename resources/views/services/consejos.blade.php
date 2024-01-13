36b5fea67ea8f2050ff6688514be55a8cd919567
Aisor Anaya Arias <aisoranaya20@gmail.com>
2024-01-12 23:52:39 -0500

    first commit

---
+++ resources/views/services/consejos.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consejos Alimenticios y Salud Mental</title>
+</head>
+<body>
+    <h1>Consejos Alimenticios y Salud Mental</h1>
+</body>
+</html>
---
+++ resources/views/services/tecnicas.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Técnicas para Controlar Ataques de Ansiedad y Pánico</title>
+</head>
+<body>
+    <h1>Técnicas para Controlar Ataques de Ansiedad y Pánico</h1>
+</body>
+</html>
